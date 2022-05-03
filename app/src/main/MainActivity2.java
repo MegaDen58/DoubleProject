7db7cd27f91521ca5ed84241fe83250b0770222e
Denis <samparizona2018@yandex.ru>
2022-05-03 23:17:26 +0300

    end

---
+++ app/src/main/MainActivity2.java
@@ -0,0 +1,42 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.AdapterView;
+import android.view.View;
+import android.content.Intent;
+import android.widget.TextView;
+
+import java.util.List;
+
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
+        super.onCreate(savedInstanceState);
+        String[] arrayInfo = getResources().getStringArray(R.array.info);
+
+
+        Intent intent = new Intent(this, third.class);
+        setContentView(R.layout.activity_main2);
+        ListView list = findViewById(R.id.info);
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, R.layout.mytextview, arrayInfo);
+
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+            {
+                intent.putExtra("key", ((TextView)v).getText() + ".html");
+                startActivity(intent);
+            }
+        });
+    }
+}
